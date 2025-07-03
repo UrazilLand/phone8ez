2a36ec58ebed4197f35960f1b4c39186e6909e18
하형진 <urazils3@gmail.com>
2025-07-04 04:13:21 +0900

    커뮤니티 게시판 및 시드 데이터/버그 수정

--- src/app/community/[tab]/write/page.tsx
+++ src/app/community/[tab]/write/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useEffect, useState } from 'react';
 import PostEditor from '../../components/PostEditor';
 import { useRouter, useParams, useSearchParams } from 'next/navigation';
